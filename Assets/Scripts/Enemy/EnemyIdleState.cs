156c019254a18c0660cde0914ad1ccac445adab1
Marcopolo Agostadero <marcopolo.agostadero@my.utsa.edu>
2025-09-23 22:17:44 -0500

    Update EnemyIdleState.cs

--- Assets/Scripts/Enemy/EnemyIdleState.cs
+++ Assets/Scripts/Enemy/EnemyIdleState.cs
@@ -2,5 +2,23 @@ using UnityEngine;
 
 public class EnemyIdleState: State<Enemy>
 {
-    
+    // function Enter is called when the State first enters
+    public void Enter()
+    {
+        // message that states the State has entered (for debugging purposes)
+        Debug.Log("Enemy has entered the Idle State");
+    }
+
+    public void Update()
+    {
+
+    }
+
+    // function Exit is called when the State leaves
+    public void Exit()
+    {
+        // message that state the State has been exited (for debugging purposes)
+        Debug.Log("Enemy has exited the Idle State");
+
+    }
 }
