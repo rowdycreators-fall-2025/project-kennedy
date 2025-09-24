1d78e12cd6d8c08d4b00318f2f4b0986037de5b5
Marcopolo Agostadero <marcopolo.agostadero@my.utsa.edu>
2025-09-23 22:11:03 -0500

    Update Enemy.cs

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    StateMachine<Enemy> enemyStateMachine;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // creates a new StateMachine for the Enemy and sets the default state
+        enemyStateMachine = GetComponent<StateMachine<Enemy>>();
+        enemyStateMachine.ChangeState(new EnemyIdleState());
     }
 
     // Update is called once per frame
