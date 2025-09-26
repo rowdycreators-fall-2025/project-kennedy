63270e9976bfbe2a34848905b83b1face2d7222c
Marcopolo Agostadero <marcopolo.agostadero@my.utsa.edu>
2025-09-26 09:44:35 -0500

    Remove Enemy.cs

--- Assets/Scripts/StateMachine/StateMachine.cs
+++ Assets/Scripts/StateMachine/StateMachine.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 // State Machine class, this class will contain the logic needed for States to change
-public class StateMachine
+public class StateMachine : MonoBehaviour
 {
     // declare the currentState variable of type State and include getters and setters
     public State currentState {  get; private set; }
