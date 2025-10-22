f69ffde3f3753aaccf34f142ad9682a4373da00d
rowdy-game-dev <kristo.cisneros-valdez@my.utsa.edu>
2025-10-22 13:08:04 -0500

    added events to shootable

--- Assets/Scripts/Gun/Shootable.cs
+++ Assets/Scripts/Gun/Shootable.cs
@@ -1,11 +1,24 @@
+using System;
 using UnityEngine;
 
 public class Shootable : MonoBehaviour
 {
     public float Health = 10f;
 
+    public delegate void Shot(float health, float damage);
+    public event Shot onShot;
+
+    public delegate void Killed();
+    public event Killed onKilled;
+
+
     public virtual void Shoot(float damage)
     {
         Debug.Log($"Shootable in {gameObject.name} got shot, damage: {damage}");
+        Health -= damage;
+        onShot?.Invoke(Health, damage);
+        if (Health <= 0) onKilled?.Invoke();
+
     }
+
 }
