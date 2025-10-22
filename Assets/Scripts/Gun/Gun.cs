0c9d81111b73071d2a26e075f72752ca885ebae9
rowdy-game-dev <kristo.cisneros-valdez@my.utsa.edu>
2025-10-22 13:07:09 -0500

    added gun properties

--- Assets/Scripts/Gun/Gun.cs
+++ Assets/Scripts/Gun/Gun.cs
@@ -2,7 +2,15 @@ using UnityEngine;
 
 public class Gun
 {
+    public float ShootDamage = 5;
+    public GameObject gunModel;
 
 
+    public Gun() { }
 
+    public Gun(float shootDamage, GameObject gunModel)
+    {
+        this.gunModel = gunModel;
+        this.ShootDamage = shootDamage;
+    }
 }
