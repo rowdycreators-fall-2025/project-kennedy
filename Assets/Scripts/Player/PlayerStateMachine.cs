5e47d7d7b2cffbf4edd957454736503de820908e
rowdy-game-dev <kristo.cisneros-valdez@my.utsa.edu>
2025-11-12 00:04:10 -0600

    Update PlayerStateMachine.cs

--- Assets/Scripts/Player/PlayerStateMachine.cs
+++ Assets/Scripts/Player/PlayerStateMachine.cs
@@ -3,8 +3,18 @@ using UnityEngine;
 
 public class PlayerStateMachine : StateMachine
 {
+    PlayerCamBob camBob;
+    public float health = 100;
+
     private void Start()
-    {
+    {   
+        camBob = GetComponent<PlayerCamBob>();
         ChangeState(new PlayerWalkState(this));
+    }
+
+    public void Hit(float damage)
+    {
+        health -= damage;
+        
     }
 }
