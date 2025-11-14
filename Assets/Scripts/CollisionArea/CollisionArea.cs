da47eb9d7c02244faaa8bd8523546d50269e1319
rowdy-game-dev <kristo.cisneros-valdez@my.utsa.edu>
2025-11-14 00:42:13 -0600

    added tag checking to collision area

--- Assets/Scripts/CollisionArea/CollisionArea.cs
+++ Assets/Scripts/CollisionArea/CollisionArea.cs
@@ -9,6 +9,8 @@ public class CollisionArea : MonoBehaviour
 
     private Collider _collider;
 
+    public string _tag = "Default";
+
     void Awake()
     {
         _collider = GetComponent<Collider>();
@@ -17,7 +19,15 @@ public class CollisionArea : MonoBehaviour
         //onAreaEntered.AddListener(() => Debug.Log("area entered"));
     }
 
-    private void OnTriggerEnter(Collider other) => onAreaEntered.Invoke();
+    private void ifMatch(Collider other, UnityEvent uEvent)
+    {
+        if (other.CompareTag(_tag))
+        {
+            uEvent.Invoke();
+        }
+    }
 
-    private void OnTriggerExit(Collider other) => onAreaExit.Invoke();
+    private void OnTriggerEnter(Collider other) => ifMatch(other, onAreaEntered);
+
+    private void OnTriggerExit(Collider other) => ifMatch(other, onAreaExit);
 }
