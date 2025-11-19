f6eef6d7cc96362b12ea42cf53dabd3ba93aca81
rowdy-game-dev <kristo.cisneros-valdez@my.utsa.edu>
2025-11-18 19:46:51 -0600

    added baby flee state

--- Assets/Scripts/BabyEnemy/BabyEnemyStateMachine.cs
+++ Assets/Scripts/BabyEnemy/BabyEnemyStateMachine.cs
@@ -2,8 +2,17 @@ using UnityEngine;
 
 public class BabyEnemyStateMachine : StateMachine
 {
+
+    public Transform _fleePoint;
+    public float _fleeSpeed = 15;
+
     protected void Start()
     {
         ChangeState(new BabyIdleState(this));
+    }
+
+    public void SetFlee()
+    {
+        ChangeState(new BabyFleeState(this));
     }
 }
